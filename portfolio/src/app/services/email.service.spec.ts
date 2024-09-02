de29383a82c0f709145b9c7b6fcc4bf853090970
kajal <122118273+Kajalshukla-ka-i@users.noreply.github.com>
2024-09-02 18:01:11 +0530

    portfolio

--- portfolio/src/app/services/email.service.spec.ts
+++ portfolio/src/app/services/email.service.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { EmailService } from './email.service';
 
 describe('EmailService', () => {
   let service: EmailService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EmailService);
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({});
+  //   service = TestBed.inject(EmailService);
+  // });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [EmailService],
+    }).compileComponents();
   });
-
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
